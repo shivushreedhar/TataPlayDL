a739a881024e0ee542453476b0b9de5cbd0c8b15
Riki <73009240+Royzzx@users.noreply.github.com>
2023-05-22 08:06:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = 17193413
+api_id = 15912260
 
-api_hash = "5e50e6380252d9d487093bae4171bf91"
+api_hash = "5c7ba9544c4d7cf3fecefebf1fd6f8bc"
 
-bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
+bot_token = "5637665860:AAFxTpPCJ-7uAcnKirFpF2cHZYgzmHxRdM4"
 
 GROUP_TAG = "RC"
 
-sudo_users = [1606667548]
+sudo_users = [533890503]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
