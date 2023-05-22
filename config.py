d0e600f6e17761a45c60adc5e2483f9ebdbd22ad
Riki <73009240+Royzzx@users.noreply.github.com>
2023-05-22 08:04:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,34 @@
+api_id = 17193413
+
+api_hash = "5e50e6380252d9d487093bae4171bf91"
+
+bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
+
+GROUP_TAG = "RC"
+
+sudo_users = [1606667548]
+
+DL_DONE_MSG = """
+✅ <b> {} Completed In </b> <code>{}</code>
+
+<b>FileName : </b> <code>{}</code>
+<b>Channel : </b> <code>{}</code>
+<b>Size : </b> <code>{}</code>
+"""
+
+
+script_developer = '''
+
+██████╗░██╗░░░██╗
+██╔══██╗╚██╗░██╔╝
+██████╦╝░╚████╔╝░
+██╔══██╗░░╚██╔╝░░
+██████╦╝░░░██║░░░
+╚═════╝░░░░╚═╝░░░
+
+██████╗░░█████╗░
+██╔══██╗██╔══██╗
+██████╔╝██║░░╚═╝
+██╔══██╗██║░░██╗
+██║░░██║╚█████╔╝
+╚═╝░░╚═╝░╚════╝'''
